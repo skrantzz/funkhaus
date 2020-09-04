d714e7ea5a7eae0663bb646a55c2aa767cdfbe21
Sydney Krantz <syd107@gmail.com>
2020-09-04 11:10:04 -0400

    looping through json data

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-fetch("db.json")
-  .then(response=> {
-      let data = response;
-    return data.json();
-  })
-  .then(data => {
-    appendData(data);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+var names = {
+    "employees": [
+        {
+        "firstName": "Aaron",
+        "lastName": "Tompkins"
+        },
+        {
+        "firstName": "Angus",
+        "lastName": "Wall"
+        }
+    ]
+}
+// alert (names.employees.length)
+for (i = 0; i < names.length; i++){
+    var output = document.getElementById('jsonData');
+    jsonData.innerHTML = names.employees[i].firstName + ' ' + names.employees[i].lastName;
+}
 
-  console.log(data);
-  var data = document.getElementById("jsonData");
-
-  for (let i = 0; i < data.length; i++) {
-
-  }
-
-  var div = document.createElement("div");
-//   div.innerHTML = "Name"
